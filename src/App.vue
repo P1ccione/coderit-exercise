899c9dd3a76a6c03c758dde90a38ad20cae79c51
P1ccione <simone.piscozzo@gmail.com>
2023-11-23 09:23:09 +0100

    alert component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <v-app>
     <Header :userIsAuthorized="userIsAuthorized"/>
+    <div class="alert-container">
+      <Alert v-show="showAlertState" :title="title" :text="text" />
+    </div>
     <v-main>
-      <router-view/>
+      <router-view @show-alert="showAlert"/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import Header from './components/Header.vue';
+import Alert from './components/Alert.vue';
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Alert,
   },
   data() {
     return {
       clientid: process.env.VUE_APP_AUTH0_CONFIG_CLIENTID,
       domain: process.env.VUE_APP_AUTH0_CONFIG_DOMAIN,
-      userIsAuthorized: this.$store.state.userIsAuthorized
+      userIsAuthorized: this.$store.state.userIsAuthorized,
+      showAlertState: false,
+      title: "",
+      text: "",
     }
   },
   beforeCreate() {
 
   },
+  methods: {
+    showAlert(alerttitle, alerttext, timeout) {
+      this.text = alerttext
+      this.title = alerttitle
+      this.showAlertState = true
+      const self = this;
+
+      setTimeout(function() {
+        self.showAlertState = false;
+      }, timeout);
+    }
+  }
 }
 </script>
+
+<style scoped> 
+.alert-container{
+  position: absolute;
+  top: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 9999999;
+} 
+</style>
